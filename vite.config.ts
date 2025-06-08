3ab9d3a72383f7378573ee2d90342594aedc88b0
raitoforce <mahgt_34@hotmail.com>
2025-06-07 20:28:17 -0600

    chore(enable @styles as alias for all the styles inside this folder): enable alias for styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@styles': path.resolve(__dirname, 'src/styles'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@styles/abstracts/variables" as *;`,
+      },
+    },
+  },
 });
